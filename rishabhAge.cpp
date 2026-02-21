da9acfa5a97d5c9e49681b4384c3fe7069cc0307
Rajan Singh <singhrajanbeawar1@gmail.com>
2026-02-21 18:23:21 +0530

    rishabh age

---
+++ rishabhAge.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"enter the age rishabh: ";
+    cin>>age;
+    if(age>18){
+        cout<<"tu to budha ho gya re ";
+    }else if(age<0) {
+        cout<<"teri gali sunna chahta h kya ";
+    }else{
+        cout<<"kya baat h tu to abhi jawan h ";
+    }
+    return 0;
+}
